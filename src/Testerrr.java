dd4670068936ac9ca02ede2151652e6193c59805
mimo <83297152+ilomimo@users.noreply.github.com>
2022-10-31 14:22:24 +0700

    Update Testerrr.java

--- src/Testerrr.java
+++ src/Testerrr.java
@@ -3,11 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package testerrr;
+package src;
 
 /**
  *
- * @author antonsumartono
  */
 public class Testerrr {
 
